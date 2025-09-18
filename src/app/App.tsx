f5c938f4740af7a21e8ca9aa00d642fca3b74d9e
PiekharievaK <pechareva@gmail.com>
2025-09-18 12:15:24 +0300

    add themeSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import type React from "react";
+import './config/theme'
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
 import { ThemeProvider } from "./providers/ThemeProvider";
--- src/app/config/theme.ts
+++ src/app/config/theme.ts
@@ -1,3 +1,12 @@
-const defaultTheme = localStorage.getItem("theme") || "light";
+const defaultTheme = localStorage.getItem("theme");
 
-document.documentElement.className = defaultTheme;
+if (defaultTheme) {
+  document.documentElement.className = defaultTheme;
+} else {
+  const systemPrefersDark = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  const systemTheme = systemPrefersDark ? "dark" : "light";
+  localStorage.setItem("theme", systemTheme);
+  document.documentElement.className = systemTheme;
+}
